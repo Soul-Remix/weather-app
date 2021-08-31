bcd6685d33e2293119cfadf094b5393b36ca847a
Soul-Remix <soulremix-webdev@pm.me>
2021-08-31 22:38:28 +0300

    Update API link

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
-const input = document.querySelector("#search");
-const searchBtn = document.querySelector("#search-btn");
-const searchContent = document.querySelector(".search-content");
-const weatherCard = document.querySelector(".weather-card");
-const h2Temp = document.querySelector(".temp");
-const pCity = document.querySelector(".city");
-const h2Forecast = document.querySelector(".forecast");
-const pForeDesc = document.querySelector(".forecast-desc");
-const feel = document.querySelector("#feel");
-const hum = document.querySelector("#hum");
-const vis = document.querySelector("#vis");
-const min = document.querySelector("#min");
-const max = document.querySelector("#max");
-const convertBtn = document.querySelector(".convert-btn");
-const backBtn = document.querySelector(".back-btn");
+const input = document.querySelector('#search');
+const searchBtn = document.querySelector('#search-btn');
+const searchContent = document.querySelector('.search-content');
+const weatherCard = document.querySelector('.weather-card');
+const h2Temp = document.querySelector('.temp');
+const pCity = document.querySelector('.city');
+const h2Forecast = document.querySelector('.forecast');
+const pForeDesc = document.querySelector('.forecast-desc');
+const feel = document.querySelector('#feel');
+const hum = document.querySelector('#hum');
+const vis = document.querySelector('#vis');
+const min = document.querySelector('#min');
+const max = document.querySelector('#max');
+const convertBtn = document.querySelector('.convert-btn');
+const backBtn = document.querySelector('.back-btn');
 
 let convert = false;
 
 async function getWeather(city) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5decbd141451f74af96dd7cedb1af758`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5decbd141451f74af96dd7cedb1af758`
     );
     const json = await response.json();
     return getData(json);
@@ -31,9 +31,9 @@ async function getWeather(city) {
 function getData(data) {
   const obj = {
     temp: +(data.main.temp - 273.15).toFixed(1),
-    feelsLike: +(data.main["feels_like"] - 273.15).toFixed(1),
-    highs: +(data.main["temp_max"] - 273.15).toFixed(1),
-    lows: +(data.main["temp_min"] - 273.15).toFixed(1),
+    feelsLike: +(data.main['feels_like'] - 273.15).toFixed(1),
+    highs: +(data.main['temp_max'] - 273.15).toFixed(1),
+    lows: +(data.main['temp_min'] - 273.15).toFixed(1),
     forecast: data.weather[0].main,
     forecastDesc: data.weather[0].description,
     humidity: data.main.humidity,
@@ -52,29 +52,29 @@ function showData(data) {
   vis.textContent = data.visibility;
   pCity.textContent = `${data.name}, ${data.country}`;
   if (convert === false) {
-    h2Temp.textContent = data.temp + "°";
-    feel.textContent = data.feelsLike + "°";
-    min.textContent = data.lows + "°";
-    max.textContent = data.highs + "°";
+    h2Temp.textContent = data.temp + '°';
+    feel.textContent = data.feelsLike + '°';
+    min.textContent = data.lows + '°';
+    max.textContent = data.highs + '°';
   } else if (convert === true) {
-    h2Temp.textContent = cToF(data.temp) + "°";
-    feel.textContent = cToF(data.feelsLike) + "°";
-    min.textContent = cToF(data.lows) + "°";
-    max.textContent = cToF(data.highs) + "°";
+    h2Temp.textContent = cToF(data.temp) + '°';
+    feel.textContent = cToF(data.feelsLike) + '°';
+    min.textContent = cToF(data.lows) + '°';
+    max.textContent = cToF(data.highs) + '°';
   }
 }
 
 function convertTemp() {
   if (convert) {
-    h2Temp.textContent = cToF(h2Temp.textContent.replace("°", "")) + "°";
-    feel.textContent = cToF(feel.textContent.replace("°", "")) + "°";
-    min.textContent = cToF(min.textContent.replace("°", "")) + "°";
-    max.textContent = cToF(max.textContent.replace("°", "")) + "°";
+    h2Temp.textContent = cToF(h2Temp.textContent.replace('°', '')) + '°';
+    feel.textContent = cToF(feel.textContent.replace('°', '')) + '°';
+    min.textContent = cToF(min.textContent.replace('°', '')) + '°';
+    max.textContent = cToF(max.textContent.replace('°', '')) + '°';
   } else {
-    h2Temp.textContent = fToC(h2Temp.textContent.replace("°", "")) + "°";
-    feel.textContent = fToC(feel.textContent.replace("°", "")) + "°";
-    min.textContent = fToC(min.textContent.replace("°", "")) + "°";
-    max.textContent = fToC(max.textContent.replace("°", "")) + "°";
+    h2Temp.textContent = fToC(h2Temp.textContent.replace('°', '')) + '°';
+    feel.textContent = fToC(feel.textContent.replace('°', '')) + '°';
+    min.textContent = fToC(min.textContent.replace('°', '')) + '°';
+    max.textContent = fToC(max.textContent.replace('°', '')) + '°';
   }
 }
 
@@ -89,47 +89,47 @@ function fToC(f) {
 }
 
 function show(el) {
-  el.style.display = "block";
+  el.style.display = 'block';
   setTimeout(function () {
-    el.style.opacity = "0.5";
+    el.style.opacity = '0.5';
   }, 100);
   setTimeout(function () {
-    el.style.transform = "translateY(0px)";
-    el.style.opacity = "1";
+    el.style.transform = 'translateY(0px)';
+    el.style.opacity = '1';
   }, 200);
 }
 
 function hide(el) {
-  el.style.display = "none";
-  el.style.transform = "translateY(-200px)";
-  el.style.opacity = "0";
+  el.style.display = 'none';
+  el.style.transform = 'translateY(-200px)';
+  el.style.opacity = '0';
 }
 
 function showOrHideError() {
-  const errorCard = document.querySelector(".error");
-  const overlay = document.querySelector(".overlay");
-  errorCard.classList.remove("hide");
-  overlay.classList.remove("hide");
-  overlay.addEventListener("click", (e) => {
-    errorCard.classList.add("hide");
-    overlay.classList.add("hide");
+  const errorCard = document.querySelector('.error');
+  const overlay = document.querySelector('.overlay');
+  errorCard.classList.remove('hide');
+  overlay.classList.remove('hide');
+  overlay.addEventListener('click', (e) => {
+    errorCard.classList.add('hide');
+    overlay.classList.add('hide');
   });
 }
 
 async function changeBackground(data) {
-  const body = document.querySelector("body");
+  const body = document.querySelector('body');
   if (data) {
     switch (data.forecast) {
-      case "Clear":
+      case 'Clear':
         body.style.backgroundImage = 'url("images/clear.jpg")';
         break;
-      case "Clouds":
+      case 'Clouds':
         body.style.backgroundImage = 'url("images/cloud.jpg")';
         break;
-      case "Rain":
+      case 'Rain':
         body.style.backgroundImage = 'url("images/rain.jpg")';
         break;
-      case "Snow":
+      case 'Snow':
         body.style.backgroundImage = 'url("images/snow.jpg")';
         break;
       default:
@@ -140,19 +140,19 @@ async function changeBackground(data) {
   }
 }
 
-searchBtn.addEventListener("click", async function (e) {
+searchBtn.addEventListener('click', async function (e) {
   e.preventDefault();
   if (input.validity.valueMissing || /[\d]/gi.test(input.value)) {
     setTimeout(function () {
-      input.style.transform = "translateX(15px)";
+      input.style.transform = 'translateX(15px)';
     }, 0);
     setTimeout(function () {
-      input.style.transform = "translateX(-15px)";
+      input.style.transform = 'translateX(-15px)';
     }, 150);
     setTimeout(function () {
-      input.style.transform = "translateX(0px)";
+      input.style.transform = 'translateX(0px)';
     }, 400);
-    input.setCustomValidity("I am expecting a city name");
+    input.setCustomValidity('I am expecting a city name');
   } else {
     const data = await getWeather(input.value);
     if (data) {
@@ -160,26 +160,26 @@ searchBtn.addEventListener("click", async function (e) {
       show(weatherCard);
       await changeBackground(data);
       showData(data);
-      input.value = "";
+      input.value = '';
     } else {
       showOrHideError();
-      input.value = "";
+      input.value = '';
     }
   }
 });
 
-backBtn.addEventListener("click", () => {
+backBtn.addEventListener('click', () => {
   hide(weatherCard);
   show(searchContent);
   changeBackground(null);
 });
 
-convertBtn.addEventListener("click", (e) => {
+convertBtn.addEventListener('click', (e) => {
   convert = !convert;
   if (convert === false) {
-    convertBtn.textContent = "C°";
+    convertBtn.textContent = 'C°';
   } else if (convert === true) {
-    convertBtn.textContent = "F°";
+    convertBtn.textContent = 'F°';
   }
   convertTemp();
 });
